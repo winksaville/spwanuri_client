021177b6322e3d5d419a9e82e1e46342efd3460d
Wink Saville <wink@saville.com>
2020-04-18 15:54:44 -0700

    Have main.dart take a SendPort as a parameter
    This allows spawnuri-client to be run from the command line or reply
    to the request via a SendPort.
    
    Enhanced the test so it tests spawnUri. I was able to valiate that it
    works but I couldn't make it work in a test. The test would just exit
    without running any tests.

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,14 @@
+import 'dart:isolate';
 import 'package:spawnuri_client/calculate.dart';
 
-void main(List<String> arguments) {
-  final int v1 = int.parse(arguments[0] ?? '6');
-  final int v2 = int.parse(arguments[1] ?? '7');
+void main(List<String> arguments, SendPort replyTo) {
+  final int v1 = int.parse((arguments.isEmpty) ? '6' : arguments[0]);
+  final int v2 = int.parse((arguments.length < 2) ? '7' : arguments[1]);
 
-  print('Hello world: ${calculate(v1, v2)}!');
+  final int result = calculate(v1, v2);
+  if (replyTo !=null) {
+    replyTo.send(result);
+  } else {
+    print('Hello world: ${calculate(v1, v2)}!');
+  }
 }
--- test/spawnuri_client_test.dart
+++ test/spawnuri_client_test.dart
@@ -1,8 +1,60 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:isolate';
+import 'package:async/async.dart';
+
 import 'package:spawnuri_client/calculate.dart';
 import 'package:test/test.dart';
 
-void main() {
+Future<void> main() async {
+
   test('calculate', () {
     expect(calculate(6, 7), 42);
   });
+
+  // TODO(wink): Make this an async test that executes
+  //
+  // According to [this](https://pub.dev/packages/test#asynchronous-tests) we
+  // should be abl3e to create asynchornous tests but I couldn't get it to
+  // work, the tests weren't run.
+
+  //test('spawnUri', () async {
+    const String v1 = '123';
+    const String v2 = '2';
+    final ReceivePort receivePort = ReceivePort();
+    final SendPort replyTo = receivePort.sendPort;
+    final Isolate isolate = await Isolate.spawnUri(Uri.parse('../bin/main.dart'), <String>[v1, v2], replyTo);
+
+    // Create aStream and feed it into a aQueue that we can
+    // wait on and know that the reply was retunred and can
+    // verify we had the expected result.
+    final StreamController<int> aStream = StreamController<int>();
+    final StreamQueue<int> aQueue = StreamQueue<int>(aStream.stream);
+
+    //receivePort.listen(expectAsync1((dynamic msg) {
+    receivePort.listen((dynamic msg) async {
+      if (msg is int) {
+        aStream.add(msg);
+      }
+    }); //, count: 1));
+
+    // Wait for the 
+    final int result = await aQueue.next;
+    final int expected = int.parse(v1) * int.parse(v2);
+    if (result == expected) {
+      print('result:$result == expected:$expected');
+    } else {
+      throw 'result:$result was not $expected';
+    }
+
+    // Cleanup queue and stream
+    aQueue.cancel();
+    aStream.close();
+
+    // Clean up receive por tand kill the isolate
+    receivePort.close();
+    isolate.kill();
+  //});
+
+  exit(0);
 }
