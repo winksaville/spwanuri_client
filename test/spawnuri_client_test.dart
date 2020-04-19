8329bb031b52393a9941e8ae2cb42bbdefd35883
Wink Saville <wink@saville.com>
2020-04-18 20:54:57 -0700

    Tests aren't working reliably
    Actually if I both 'Test spawnUri' and 'Inline spawnUri' test
    enabled then about 1 out of 3 or 4 times they all finish:
    
      wink@wink-desktop:~/prgs/dart/spawnuri-client (master)
      $ make test
      dart test/spawnuri_client_test.dart
      Inline spawnUri:+
      00:00 +0: calculate 6 * 7
    
      00:00 +1: calculate -1 * 7
    
      00:00 +2: Test spawnUri
    
      Test spawnUri:+
    
      Test spawnUri: 1
    
      Inline spawnUri:-
      wink@wink-desktop:~/prgs/dart/spawnuri-client (master)
      $ make test
      dart test/spawnuri_client_test.dart
      Inline spawnUri:+
      00:00 +0: calculate 6 * 7
    
      00:00 +1: calculate -1 * 7
    
      00:00 +2: Test spawnUri
    
      Test spawnUri:+
    
      Test spawnUri: 1
    
      Inline spawnUri:-
      wink@wink-desktop:~/prgs/dart/spawnuri-client (master)
      $ make test
      dart test/spawnuri_client_test.dart
      Inline spawnUri:+
      00:00 +0: calculate 6 * 7
    
      00:00 +1: calculate -1 * 7
    
      00:00 +2: Test spawnUri
    
      Test spawnUri:+
    
      Test spawnUri: 1
    
      Test spawnUri: 2
    
      Test spawnUri: 4
    
      Test spawnUri: 3
    
      Test spawnUri: 5
    
      Test spawnUri:-
    
      00:00 +3: All tests passed!
    
      Inline spawnUri:-
    
    If I have just 'Test spawnUri' I haven't seen any output:
    
      wink@wink-desktop:~/prgs/dart/spawnuri-client (master)
      $ make test
      dart test/spawnuri_client_test.dart
      wink@wink-desktop:~/prgs/dart/spawnuri-client (master)
      $ make test
      dart test/spawnuri_client_test.dart
      wink@wink-desktop:~/prgs/dart/spawnuri-client (master)
      $ make test
      dart test/spawnuri_client_test.dart
      wink@wink-desktop:~/prgs/dart/spawnuri-client (master)
      $ make test
      dart test/spawnuri_client_test.dart
    
    And if I have just 'Inline sapwnUri' enabled the enabled tests work as
    expected:
    
      $ make test
      dart test/spawnuri_client_test.dart
      Inline spawnUri:+
      00:00 +0: calculate 6 * 7
    
      00:00 +1: calculate -1 * 7
    
      00:00 +2: All tests passed!
    
      Inline spawnUri:-

--- test/spawnuri_client_test.dart
+++ test/spawnuri_client_test.dart
@@ -6,21 +6,75 @@ import 'package:async/async.dart';
 import 'package:spawnuri_client/calculate.dart';
 import 'package:test/test.dart';
 
+//void main() {
 Future<void> main() async {
+  const String v1 = '123';
+  const String v2 = '2';
+  final int expected = int.parse(v1) * int.parse(v2);
 
-  test('calculate', () {
+  test('calculate 6 * 7', () {
     expect(calculate(6, 7), 42);
   });
 
-  // TODO(wink): Make this an async test that executes
+  test('calculate -1 * 7', () {
+    expect(calculate(-1, 7), -7);
+  });
+
+  // TODO(wink): Make this an async test work reliabely.
   //
   // According to [this](https://pub.dev/packages/test#asynchronous-tests) we
-  // should be abl3e to create asynchornous tests but I couldn't get it to
-  // work, the tests weren't run.
+  // should beable to create asynchornous tests. But I couldn't get this to
+  // work reliablely.
+  //
+  // If both are disabled then we always see the two calcuate tests.
+  //
+  // If 'Test spawnUri is enabled and 'Inline spawnUri' is disabled then there
+  // is no output.
+  //
+  // If 'Test spwanUri is disabled and 'Inline spawnUri' calcuate tests and
+  // 'Inline spawnUri' work.
+  //
+  // If both 'Test spawnUri' and 'Inline spawnUri' are enabled then 1 out of 4
+  // times it works as expected, but the other times 'Test spawnUri' starts but
+  // only outputs upto 'Test spawnUri: 1' and 'Inline spawnUri' works everytime.
+  //
+  // This above is on my desktop, YMMV :)
+  if (false) {
+    test('Test spawnUri', () async {
+      print('Test spawnUri:+');
+      final ReceivePort receivePort = ReceivePort();
+      final SendPort replyTo = receivePort.sendPort;
+      print('Test spawnUri: 1');
+      final Isolate isolate = await Isolate.spawnUri(Uri.parse('../bin/main.dart'), <String>[v1, v2], replyTo);
+      print('Test spawnUri: 2');
 
-  //test('spawnUri', () async {
-    const String v1 = '123';
-    const String v2 = '2';
+      final StreamController<int> aStream = StreamController<int>();
+      final StreamQueue<int> aQueue = StreamQueue<int>(aStream.stream);
+
+      //receivePort.listen(expectAsync1<Function, dynamic>((dynamic msg) {
+      receivePort.listen((dynamic msg) {
+        print('Test spawnUri: 3');
+        if (msg is int) {
+          expect(msg, expected);
+          aStream.add(msg);
+        }
+      });
+      //}, count: 1));
+
+      print('Test spawnUri: 4');
+      final int result = await aQueue.next;
+      print('Test spawnUri: 5');
+      expect(result, expected);
+
+      // Clean up receive port and kill the isolate
+      receivePort.close();
+      isolate.kill();
+      print('Test spawnUri:-');
+    });
+  }
+
+  if (true) {
+    print('Inline spawnUri:+');
     final ReceivePort receivePort = ReceivePort();
     final SendPort replyTo = receivePort.sendPort;
     final Isolate isolate = await Isolate.spawnUri(Uri.parse('../bin/main.dart'), <String>[v1, v2], replyTo);
@@ -31,30 +85,29 @@ Future<void> main() async {
     final StreamController<int> aStream = StreamController<int>();
     final StreamQueue<int> aQueue = StreamQueue<int>(aStream.stream);
 
-    //receivePort.listen(expectAsync1((dynamic msg) {
     receivePort.listen((dynamic msg) async {
       if (msg is int) {
         aStream.add(msg);
       }
-    }); //, count: 1));
+    });
 
     // Wait for the 
     final int result = await aQueue.next;
-    final int expected = int.parse(v1) * int.parse(v2);
+
     if (result == expected) {
-      print('result:$result == expected:$expected');
     } else {
-      throw 'result:$result was not $expected';
+      throw 'Inline spawnUri: result:$result was not $expected';
     }
 
     // Cleanup queue and stream
     aQueue.cancel();
     aStream.close();
 
-    // Clean up receive por tand kill the isolate
+    // Clean up receive port and kill the isolate
     receivePort.close();
     isolate.kill();
-  //});
+    print('Inline spawnUri:-');
+  }
 
   exit(0);
 }
